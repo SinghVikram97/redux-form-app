a2074133d2f5e9682602cd134d6040376affca98
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2020-08-17 17:45:34 +0530

    Added onSubmit handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+import { Field, reduxForm, formValues } from "redux-form";
 function App(props) {
+  // console.log(props); // many functions and values
+  // handleSubmit also in props
+  const { handleSubmit } = props;
+  // console.log(String(handleSubmit));
   return (
     <div className="App">
       <h1>Redux-Form</h1>
-      <label>First Name: </label>
-      <Field type="text" name="firstName" component="input" />
-      <br />
-      <label>Last Name: </label>
-      <Field type="text" name="lastName" component="input" />
+      <form
+        onSubmit={handleSubmit(formValues => {
+          // console.log(formValues);  {firstName: "vikram", lastName: "bedi"}
+        })}
+      >
+        <label>First Name: </label>
+        <Field type="text" name="firstName" component="input" />
+        <br />
+        <label>Last Name: </label>
+        <Field type="text" name="lastName" component="input" />
+        <br />
+        <button type="submit">Submit it</button>
+      </form>
     </div>
   );
 }
