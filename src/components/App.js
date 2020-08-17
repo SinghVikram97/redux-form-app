3596967028edb2cc04683c3ede3484c50b25688b
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2020-08-17 17:32:30 +0530

    Added fields and data is getting stored in redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,19 @@
-import React, { useState } from "react";
-import { connect } from "react-redux";
-import { saveFormDetails } from "../actions/form-action";
+import React from "react";
+import { Field, reduxForm } from "redux-form";
 function App(props) {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
   return (
     <div className="App">
       <h1>Redux-Form</h1>
-      First Name:
-      <input
-        type="text"
-        name="firstName"
-        value={firstName}
-        onChange={e => setFirstName(e.target.value)}
-      />
+      <label>First Name: </label>
+      <Field type="text" name="firstName" component="input" />
       <br />
-      Last Name:
-      <input
-        type="text"
-        name="lastName"
-        value={lastName}
-        onChange={e => setLastName(e.target.value)}
-      />
-      <br />
-      <button onClick={() => props.submitForm(firstName, lastName)}>
-        Save
-      </button>
-      <br />
-      <br />
-      <p>First Name: {firstName}</p>
-      <p>Last Name: {lastName}</p>
+      <label>Last Name: </label>
+      <Field type="text" name="lastName" component="input" />
     </div>
   );
 }
-const mapDispatchToProps = dispatch => {
-  return {
-    submitForm(firstName, lastName) {
-      dispatch(saveFormDetails(firstName, lastName));
-    }
-  };
-};
 
-export default connect(null, mapDispatchToProps)(App);
+// reduxForm similar to connect
+export default reduxForm({
+  form: "myForm" // unique name
+})(App);
